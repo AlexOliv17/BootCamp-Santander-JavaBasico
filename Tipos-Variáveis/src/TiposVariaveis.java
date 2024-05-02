981d5a471e110935fdcd1ed2964eb6c438749efe
Alex Oliveira <alexoalves10@gmail.com>
2024-05-02 18:22:46 -0300

    Explicação de variáveis com o método final na sua declaração.

--- Tipos-Variáveis/src/TiposVariaveis.java
+++ Tipos-Variáveis/src/TiposVariaveis.java
@@ -1,6 +1,26 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*
+         * O método "final" serve para variáveis que terão
+         * o mesmo valor sempre, sem trocas. Sua sintaxe fica:
+         */
+
+        final double VALOR_DE_PI = 3.14;
+
+        /*
+         * Caso tente mudar seu valor:
+         * valorDePi = 5;
+         * 
+         * O código apontará erro, pois variáveis declaradas com
+         * final nao podem ter seu valor mudado.
+         * 
+         * A sintaxe de uma variavel que possui final na sua declaração
+         * também deve ser feita em caixa alta, como padrão usado no JAVA
+         * para esse tipo de variável. exemplo:
+         * 
+         * final double VALOR_DE_PI = 3.14;
+         */
+
     }
 }
