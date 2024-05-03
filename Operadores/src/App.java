e1847d8f0bda3f1c67679a24066fa76a5f023f60
Alex Oliveira <alexoalves10@gmail.com>
2024-05-03 20:55:14 -0300

    Explicação de Operadores Ternários!

--- Operadores/src/App.java
+++ Operadores/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int number1 = 6;
+        int number2 = 6;
+
+        /*
+         * O operador ternario funcionar como um if resumido.
+         * ele usa a propria declaração da variavel para já determinar
+         * se aquela operação é VERDADEIRA ou FALSA.
+         * A sintaxe fica assim:
+         */
+
+        String resultado = number1 == number2 ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
     }
 }
