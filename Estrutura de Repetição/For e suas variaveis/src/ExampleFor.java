05bf0f23832c8f9b447bfd22f7d1d68fd8956b37
Alex Oliveira <alexoalves10@gmail.com>
2024-05-08 18:02:31 -0300

    Exemplo da estrutura do for: Contador de carneirinhos.

--- Estrutura de Repetição/For e suas variaveis/src/ExampleFor.java
+++ Estrutura de Repetição/For e suas variaveis/src/ExampleFor.java
@@ -1,6 +1,9 @@
 public class ExampleFor {
 
+    // Código simples com intuíto explicativo da estrutura for:
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for (int carneirinho = 0; carneirinho <= 20; carneirinho++) {
+            System.out.println("Carneirinho n°: " + carneirinho);
+        }
     }
 }
