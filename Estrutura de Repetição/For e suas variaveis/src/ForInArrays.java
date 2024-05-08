43064a2cf31d99fe49f50dc3226f90f094103944
Alex Oliveira <alexoalves10@gmail.com>
2024-05-08 18:10:01 -0300

    Exemplo de For em Arrays

---
+++ Estrutura de Repetição/For e suas variaveis/src/ForInArrays.java
@@ -0,0 +1,9 @@
+public class ForInArrays {
+
+    public static void main(String[] args) {
+        String alunos[] = { "Alex", "Oliveira", "Alves" };
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x = " + x + " é o(a) " + alunos[x]);
+        }
+    }
+}
